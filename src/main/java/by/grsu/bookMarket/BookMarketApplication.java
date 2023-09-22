c15b069ee411d1a734d2593156cd5ba69daae223
Alexander Savitskiy <64911519+Cr1stalGG@users.noreply.github.com>
2023-09-22 22:32:46 +0300

    Main class

---
+++ src/main/java/by/grsu/bookMarket/BookMarketApplication.java
@@ -0,0 +1,13 @@
+package by.grsu.bookMarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookMarketApplication.class, args);
+	}
+
+}
