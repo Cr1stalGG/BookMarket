aa233fe77517e75500a3228d67b5d0d9b8b5d1d7
Alexander Savitskiy <64911519+Cr1stalGG@users.noreply.github.com>
2023-09-23 10:33:47 +0300

    Add query to find books

--- src/main/java/by/grsu/bookMarket/repository/BookRepository.java
+++ src/main/java/by/grsu/bookMarket/repository/BookRepository.java
@@ -2,13 +2,14 @@ package by.grsu.bookMarket.repository;
 
 import by.grsu.bookMarket.entity.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface BookRepository extends JpaRepository<Book, Long> {
-   // @Query(value = "") //TODO add query to find all books by smth looks like name and grouped by name
+   @Query(value = "select b from book b where b.name like %:#{#query}% order by b.name", nativeQuery = true) //TODO add query to find all books by smth looks like name and grouped by name
     List<Book> find(String query);
     Book findBookByName(String name);
     void deleteBookByName(String name);
