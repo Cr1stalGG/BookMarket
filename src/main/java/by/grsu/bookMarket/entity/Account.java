7093be5710bb3fc24f2426d3b003ce1b32227700
Alexander Savitskiy <64911519+Cr1stalGG@users.noreply.github.com>
2023-09-26 22:41:46 +0300

    Revert "Add authentication and registration"
    This reverts commit 7aa5c9b2

--- src/main/java/by/grsu/bookMarket/entity/Account.java
+++ src/main/java/by/grsu/bookMarket/entity/Account.java
@@ -3,22 +3,26 @@ package by.grsu.bookMarket.entity;
 
 import by.grsu.bookMarket.entity.enumirations.Role;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
-import lombok.*;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-import java.util.Collection;
 import java.util.List;
 
 @Entity
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-@EqualsAndHashCode(callSuper = true)
 @Data
-public class Account extends BaseEntity<Long> implements UserDetails {
+public class Account {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
     private String mail;
     private String password;
     private double amount;
@@ -34,40 +38,4 @@ public class Account extends BaseEntity<Long> implements UserDetails {
     public void addAmount(double amount) {
         this.amount += amount;
     }
-
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of(new SimpleGrantedAuthority(this.role.name()));
-    }
-
-    @Override
-    public String getUsername() {
-        return this.mail;
-    }
-
-    @Override
-    public String getPassword(){
-        return this.password;
-    }
-
-    @Override
-    public boolean isAccountNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isAccountNonLocked() {
-        return true;
-    }
-
-    @Override
-    public boolean isCredentialsNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isEnabled() {
-        return true;
-    }
 }
-
