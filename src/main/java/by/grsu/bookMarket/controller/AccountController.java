a0f02f740f7a7a66e5a89e1249294c07b22c543e
Alexander Savitskiy <64911519+Cr1stalGG@users.noreply.github.com>
2023-09-26 23:44:22 +0300

    Refactoring

--- src/main/java/by/grsu/bookMarket/controller/AccountController.java
+++ src/main/java/by/grsu/bookMarket/controller/AccountController.java
@@ -2,8 +2,6 @@ package by.grsu.bookMarket.controller;
 
 import by.grsu.bookMarket.entity.dto.accountDTO.AccountAddAmountRequest;
 import by.grsu.bookMarket.entity.dto.accountDTO.AccountMainInfoDTO;
-import by.grsu.bookMarket.entity.dto.authorDTO.AuthorCreationDTO;
-import by.grsu.bookMarket.entity.dto.bookDTO.BookCreationDTO;
 import by.grsu.bookMarket.service.AccountServiceImpl;
 import by.grsu.bookMarket.service.AdminServiceImpl;
 import lombok.RequiredArgsConstructor;
@@ -25,28 +23,8 @@ public class AccountController {
         return accountService.addAmount(addAmountRequest);
     }
 
-    @PostMapping("/buy-book/{bookName}")
+    @PostMapping("/book/{bookName}")
     public String buyBook(@PathVariable String mail, @PathVariable String bookName){
         return accountService.buyBook(mail, bookName);
-    }
-
-    @PostMapping("/add-author")
-    public void addAuthor(@RequestBody AuthorCreationDTO authorDTO){
-        adminService.addAuthor(authorDTO);
-    }
-
-    @DeleteMapping("/delete-author/{authorName}")
-    public void deleteAuthor(@PathVariable String authorName){
-        adminService.deleteAuthor(authorName);
-    }
-
-    @PostMapping("/add-book")
-    public void addBook(@RequestBody BookCreationDTO bookDTO){
-        adminService.addBook(bookDTO);
-    }
-
-    @DeleteMapping("/delete-book/{bookName}")
-    public void deleteBook(@PathVariable String bookName){
-        adminService.deleteBook(bookName);
     }
 }
--- src/main/java/by/grsu/bookMarket/controller/AdminController.java
+++ src/main/java/by/grsu/bookMarket/controller/AdminController.java
@@ -12,22 +12,22 @@ import org.springframework.web.bind.annotation.*;
 public class AdminController {
     private final AdminServiceImpl adminService;
 
-    @PostMapping("/add-author")
+    @PostMapping("/author")
     public void addAuthor(@RequestBody AuthorCreationDTO authorDTO){
         adminService.addAuthor(authorDTO);
     }
 
-    @DeleteMapping("/delete-author/{authorName}")
+    @DeleteMapping("/author/{authorName}")
     public void deleteAuthor(@PathVariable String authorName){
         adminService.deleteAuthor(authorName);
     }
 
-    @PostMapping("/add-book")
+    @PostMapping("/book")
     public void addBook(@RequestBody BookCreationDTO bookDTO){
         adminService.addBook(bookDTO);
     }
 
-    @DeleteMapping("/delete-book/{bookName}")
+    @DeleteMapping("/book/{bookName}")
     public void deleteBook(@PathVariable String bookName){
         adminService.deleteBook(bookName);
     }
