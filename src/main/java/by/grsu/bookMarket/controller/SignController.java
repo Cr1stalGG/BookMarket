4dfd02b1fb8008d77ce3d1a113b338b5efd1bf59
Alexander Savitskiy <64911519+Cr1stalGG@users.noreply.github.com>
2023-09-26 22:39:50 +0300

    Revert "Delete useless class"
    This reverts commit b028aa4a5e1e005934afc6b66a223010e2ee220a.

--- src/main/java/by/grsu/bookMarket/controller/SignController.java
+++ src/main/java/by/grsu/bookMarket/controller/SignController.java
@@ -3,6 +3,7 @@ package by.grsu.bookMarket.controller;
 import by.grsu.bookMarket.security.authDTO.AccountAuthRequest;
 import by.grsu.bookMarket.security.authDTO.AccountAuthResponse;
 import by.grsu.bookMarket.service.AccountServiceImpl;
+import by.grsu.bookMarket.service.AddAminService_Test;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class SignController {
     private final AccountServiceImpl accountService;
+    private final AddAminService_Test addAminService;
 
     @PostMapping("/up")
     public AccountAuthResponse registration(@RequestBody AccountAuthRequest request){
@@ -23,5 +25,10 @@ public class SignController {
     @PostMapping("/in")
     public AccountAuthResponse authentication(@RequestBody AccountAuthRequest request){
         return accountService.authentication(request);
+    }
+
+    @PostMapping("/addAdmin")
+    public void addAdmin(){
+        addAminService.addAdmin();
     }
 }
